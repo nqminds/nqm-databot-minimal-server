d9d501a6dee208cdb4e0a93d123e79ebdf2103c6
toby ealden <toby.ealden@gmail.com>
2016-09-17 20:55:41 +0100

    bump to version 0.1.0

--- index.js
+++ index.js
@@ -1,70 +1,63 @@
-if (!process.env.DEBUG) {
-  process.env.DEBUG = "nqm-*";
-}
-
-var express = require("express");
-var input = require("nqm-process-utils").input;
-var output = require("nqm-process-utils").output;
-var security = require("nqm-process-utils").security;
-
-// Retrieve the input data sent to us from the host.
-input.read(function(err, inputArgs) {
-  if (err) {
-    // Report errors back to the host.
-    output.error("failure reading input: %s", err.message);
-    // Exit with non-zero code to indicate failure. 
-    process.exit(1);
+(function() {
+  if (!process.env.DEBUG) {
+    process.env.DEBUG = "nqm-*";
   }
 
-  //
-  // Validate the input.
-  //
-  // This databot expects input data using the following schema:
-  //
-  // {
-  //   title: "<title here>",
-  //   message: "<message here>",
-  //   answer: "<answer here>"
-  // }
-  //
-  if (!inputArgs.data.title || !inputArgs.data.message || !inputArgs.data.answer) {
-    output.error("invalid args");
-    process.exit(1);
-  }
+  var express = require("express");
+  var input = require("nqm-process-utils").input;
+  var security = require("nqm-process-utils").security;
 
-  // Create and initialise an express server.
-  var app = express();
-  app.set("view engine", "pug");
-  app.use(express.static('public'));
+  function databot(input, output, context) {
+    //
+    // Validate the input.
+    //
+    // This databot expects input data using the following schema:
+    //
+    // {
+    //   title: "<title here>",
+    //   message: "<message here>",
+    //   answer: "<answer here>"
+    // }
+    //
+    if (!input.title || !input.message || !input.answer) {
+      output.error("invalid args");
+      process.exit(1);
+    }
 
-  // Make sure all requests originate from the host.
-  app.use(security.authoriseRequest(inputArgs.instanceAuthKey));
+    // Create and initialise an express server.
+    var app = express();
+    app.set("view engine", "pug");
+    app.use(express.static("public"));
 
-  // Example view rendering.
-  app.get("/home", function(req,res) {
-    // Respond with the title and message passed as input to this databot
-    res.render("home", {title: inputArgs.data.title || "server process", message: inputArgs.data.message || "hello"});
-  });
+    // Make sure all requests originate from the host.
+    app.use(security.authoriseRequest(inputArgs.instanceAuthKey));
 
-  // Example json response.
-  app.get("/json", function(req,res) {
-    res.json({
-      answer: inputArgs.data.answer || 123,
-      message: "testing JSON response type"
+    // Example view rendering.
+    app.get("/home", function(req,res) {
+      // Respond with the title and message passed as input to this databot
+      res.render("home", {title: input.title || "server process", message: input.message || "hello"});
     });
-  });
 
-  app.get("/exit", function(req, res) {
-    res.json({ok: "goodbye!"});
-    
-    setTimeout(function() {
-      output.debug("exited at user request");
-      process.exit(0);
-    }, 1000);
-  });
+    // Example json response.
+    app.get("/json", function(req,res) {
+      res.json({
+        answer: input.answer || 123,
+        message: "testing JSON response type"
+      });
+    });
 
-  // Use the port supplied to us by the process host.
-  app.listen(inputArgs.instancePort, function() {
+    // Instruct the databot to stop.
+    app.get("/exit", function(req, res) {
+      res.json({ok: "goodbye!"});
+      
+      setTimeout(function() {
+        output.debug("exited at user request");
+        process.exit(0);
+      }, 1000);
+    });
+
+    // Use the port supplied to us by the process host.
+    app.listen(inputArgs.instancePort, function() {
       output.debug("listening on %d", inputArgs.instancePort);
       output.progress(0);
     })
@@ -73,5 +66,8 @@ input.read(function(err, inputArgs) {
       process.exit(-1);
     })
     .on("connection", security.authoriseConnection);
-});
+  }
 
+  // Retrieve the input data sent to us from the host, and pipe it to the databot function.
+  input.pipe(databot);
+}());
